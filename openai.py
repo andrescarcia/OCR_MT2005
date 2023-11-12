61c8adfa700c11626967855e71b6353c32f0fe0e
andrescarcia <49753315+andrescarcia@users.noreply.github.com>
2023-11-12 01:29:02 -0400

    OpenAI Script
    Esta es la subida de un script para usar GPT-4 Vision, para procesar las imagenes, hasta ahora el modelo que mejor da respuesta.

---
+++ openai.py
@@ -0,0 +1,85 @@
+import os
+import base64
+import requests
+import pandas as pd
+from tqdm import tqdm
+
+'''
+lista de comandos para instalar las librerias necesarias
+pip install pandas requests tqdm
+pip install openai
+
+'''
+# Configura tu clave API de OpenAI
+api_key = "YOUR_API_KEY"
+
+def leer_imagenes(carpeta):
+    archivos = []
+    for archivo in os.listdir(carpeta):
+        if archivo.endswith('.jpg') or archivo.endswith('.png'):
+            archivos.append(archivo)
+    return archivos
+
+def encode_image(image_path):
+    with open(image_path, "rb") as image_file:
+        return base64.b64encode(image_file.read()).decode('utf-8')
+
+def procesar_imagenes(archivos, carpeta):
+    dataset = []
+    for archivo in tqdm(archivos, desc="Procesando imágenes"):
+        print(f"Procesando {archivo}...")
+        image_path = f"{carpeta}/{archivo}"
+        base64_image = encode_image(image_path)
+
+        headers = {
+            "Content-Type": "application/json",
+            "Authorization": f"Bearer {api_key}"
+        }
+
+        payload = {
+            "model": "gpt-4-vision-preview",
+            "messages": [
+                {
+                    "role": "user",
+                    "content": [
+                        {
+                            "type": "text",
+                            "text": "This image a number, i need you to extract it and send it to me, only respond with the number"
+                        },
+                        {
+                            "type": "image_url",
+                            "image_url": {
+                                "url": f"data:image/jpeg;base64,{base64_image}"
+                            }
+                        }
+                    ]
+                }
+            ],
+            "max_tokens": 300
+        }
+
+        response = requests.post("https://api.openai.com/v1/chat/completions", headers=headers, json=payload)
+        if response.status_code == 200:
+            respuesta = response.json()
+            texto = respuesta['choices'][0]['message']['content']
+            dataset.append({'imagen': archivo, 'texto': texto})
+        else:
+            print(f"Error procesando {archivo}: {response.status_code}")
+
+    return dataset
+
+
+def guardar_en_excel(dataset, nombre_archivo):
+    df = pd.DataFrame(dataset)
+    df.to_excel(nombre_archivo, index=False)
+
+# Ruta de la carpeta donde están las imágenes y donde se guardará el Excel
+carpeta_imagenes = r'C:\Users\andre\Desktop\openai'
+ruta_excel = r'C:\Users\andre\Desktop\openai\dataset.xlsx'
+
+# Procesar
+archivos = leer_imagenes(carpeta_imagenes)
+dataset = procesar_imagenes(archivos, carpeta_imagenes)
+guardar_en_excel(dataset, ruta_excel)
+
+print(f"Proceso completado. El dataset se ha guardado en '{ruta_excel}'.")
--- preprocessing.py
+++ preprocessing.py
@@ -1,3 +1,39 @@
+
+
+
+import cv2
+import numpy as np
+import pytesseract
+import re
+
+class Preprocessor:
+    @staticmethod
+    def preprocess_image(image_path, scale_factor=1.5):
+        # Leer la imagen desde el archivo
+        img = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
+        # Cambiar el tamaño de la imagen (escalar)
+        width = int(img.shape[1] * scale_factor)
+        height = int(img.shape[0] * scale_factor)
+        img = cv2.resize(img, (width, height), interpolation=cv2.INTER_CUBIC)
+        # Preprocesamiento de la imagen escalada
+        _, img = cv2.threshold(img, 128, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
+        
+        # Intentar detectar la orientación del texto
+        try:
+            osd = pytesseract.image_to_osd(img)
+            angle = int(re.search(r'(?<=Rotate: )\d+', osd).group(0))
+            if angle != 0:
+                center = (width // 2, height // 2)
+                M = cv2.getRotationMatrix2D(center, angle, 1.0)
+                img = cv2.warpAffine(img, M, (width, height), flags=cv2.INTER_CUBIC, borderMode=cv2.BORDER_REPLICATE)
+        except (pytesseract.pytesseract.TesseractError, ValueError) as e:
+            print(f"No se pudo obtener la orientación de la imagen {image_path}. Error: {e}")
+            # Aquí podrías implementar una rotación alternativa basada en la detección de bordes si fuera necesario.
+
+        return img
+
+
+
 """
 import cv2
 import numpy as np
@@ -68,36 +104,3 @@ def upscale_image(image_path, scale_factor):
 # Usar la función y guardar la imagen escalada
 #upscaled_image = upscale_image('tu_imagen.jpg', 2) # Cambiar el factor de escala según sea necesario
 #cv2.imwrite('imagen_escalada.jpg', upscaled_image)
-
-
-
-import cv2
-import numpy as np
-import pytesseract
-import re
-
-class Preprocessor:
-    @staticmethod
-    def preprocess_image(image_path, scale_factor=1.5):
-        # Leer la imagen desde el archivo
-        img = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
-        # Cambiar el tamaño de la imagen (escalar)
-        width = int(img.shape[1] * scale_factor)
-        height = int(img.shape[0] * scale_factor)
-        img = cv2.resize(img, (width, height), interpolation=cv2.INTER_CUBIC)
-        # Preprocesamiento de la imagen escalada
-        _, img = cv2.threshold(img, 128, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
-        
-        # Intentar detectar la orientación del texto
-        try:
-            osd = pytesseract.image_to_osd(img)
-            angle = int(re.search('(?<=Rotate: )\d+', osd).group(0))
-            if angle != 0:
-                center = (width // 2, height // 2)
-                M = cv2.getRotationMatrix2D(center, angle, 1.0)
-                img = cv2.warpAffine(img, M, (width, height), flags=cv2.INTER_CUBIC, borderMode=cv2.BORDER_REPLICATE)
-        except (pytesseract.pytesseract.TesseractError, ValueError) as e:
-            print(f"No se pudo obtener la orientación de la imagen {image_path}. Error: {e}")
-            # Aquí podrías implementar una rotación alternativa basada en la detección de bordes si fuera necesario.
-
-        return img
